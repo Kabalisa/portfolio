e3dc63a5b57be7e4c9d9492fd9b42014483ed8f4
Innocent Fiston Kabalisa <ikabalisa20@gmail.com>
2020-04-11 00:16:49 +0200

    about me page

---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Menu } from './Menu';
+import Profile from '../assets/images/innocent fiston kabalisa (2).jpg';
+
+const AboutMe = () => {
+  return (
+    <div className='AboutMe'>
+      <Menu />
+      <div className='ProfileImage'>
+        <img className='ProfileImg' alt='profile' src={Profile} />
+      </div>
+      <div className='TextContainer'>
+        <p className='AboutMeText'>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum
+        </p>
+      </div>
+      <div className='SkillsContainer'>
+        <span>Skills:</span>
+        <div className='Skills'>
+          <span>Frontend Skills</span>
+          <ul>
+            <li>that time</li>
+            <li>that time</li>
+            <li>that time</li>
+            <li>that time</li>
+            <li>that time</li>
+            <li>that time</li>
+            <li>that time</li>
+            <li>that time</li>
+          </ul>
+          <span>Backend Skills</span>
+          <ul>
+            <li>lower body</li>
+            <li>lower body</li>
+            <li>lower body</li>
+            <li>lower body</li>
+            <li>lower body</li>
+          </ul>
+          <span>Project management</span>
+          <ul>
+            <li>indigo</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export { AboutMe };
--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
 import React, { Fragment } from 'react';
 import '../assets/css/App.css';
 import { Welcome } from './Welcome';
-import {Projects} from './Projects';
+import { Projects } from './Projects';
+import { AboutMe } from './AboutMe';
 
 const App = () => {
   return (
     <Fragment>
       <Welcome />
       <Projects />
+      <AboutMe />
     </Fragment>
   );
 };
