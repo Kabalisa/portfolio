9a9143009b97d720ffe4f23d02d4006860269671
Kabalisa <ikabalisa20@gmail.com>
2020-05-19 12:02:49 +0200

    desktop styles menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import menuIcon from '../assets/images/menu.png';
 
 const Menu = ({ Open }) => {
+  const [width, setWidth] = useState(window.innerWidth);
+
+  const updateWidth = () => setWidth(window.innerWidth);
+
+  useEffect(() => {
+    window.addEventListener('resize', updateWidth);
+    return () => window.addEventListener('resize', updateWidth);
+  });
+
+  console.log('===let see now', width);
+
   return (
     <div className='menu'>
       <span className='name'>Innocent Fiston Kabalisa</span>
+      {width > 768 ? <span className='MenuOptions' >Projects</span> : <Fragment>
       <span className='hambuger' onClick={Open}>
         <img alt='hambuger button' src={menuIcon} />
       </span>
+      </Fragment>}
     </div>
   );
 };
