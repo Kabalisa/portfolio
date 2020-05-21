3228404e2c71392ff682584826330567673299fa
Kabalisa <ikabalisa20@gmail.com>
2020-05-21 10:21:16 +0200

    desktop styles about me

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -4,9 +4,10 @@ import Profile from '../assets/images/innocent fiston kabalisa (2).jpg';
 const AboutMe = () => {
   return (
     <div className='AboutMe'>
+      <div className='ImageText' >
       <img className='ProfileImg' alt='profile' src={Profile} />
-      <div className='TextContainer'>
-        <p className='AboutMeText'>
+      <div className='TextContainer' >
+       <p className='AboutMeText'>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
           minim veniam, quis nostrud exercitation ullamco laboris nisi ut
@@ -15,6 +16,7 @@ const AboutMe = () => {
           pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
           culpa qui officia deserunt mollit anim id est laborum
         </p>
+      </div>
       </div>
       <div className='SkillsContainer'>
         <span>Skills:</span>
